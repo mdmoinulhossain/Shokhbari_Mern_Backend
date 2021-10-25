d5ba5b3c30f793d3f1a1bda673f7ff24e7c5a89d
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2021-10-25 12:38:09 +0600

    signup & setup

---
+++ src/index.server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+
+const useRoute = require("./routes/user");
+//environment variables or constants
+env.config();
+
+//mongoDB connection
+//mongodb+srv://root:<password>@cluster0.u7cx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.u7cx5.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("database connected successfully");
+  });
+
+app.use(express.json());
+app.use("/api", useRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running port ${process.env.PORT}`);
+});
