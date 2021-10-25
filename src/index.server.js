66f055fc510fbebca4a78e6f15437e97dc53c357
unknown <za0162hid@gmail.com>
2021-10-25 18:09:08 +0600

    signin adding

--- src/index.server.js
+++ src/index.server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const env = require("dotenv");
 const mongoose = require("mongoose");
-const app = express();
 
-const useRoute = require("./routes/user");
+const app = express();
+const authRoutes = require("./routes/auth");
 //environment variables or constants
 env.config();
 
 //mongoDB connection
 //mongodb+srv://root:<password>@cluster0.u7cx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.u7cx5.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
@@ -19,7 +18,7 @@ mongoose
   });
 
 app.use(express.json());
-app.use("/api", useRoute);
+app.use("/api", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running port ${process.env.PORT}`);
--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,11 @@ userSchema.virtual("password").set(function (password) {
   this.hash_password = bcrypt.hashSync(password, 10);
 });
 
+//this is for the full name
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
 userSchema.methods = {
   authenticate: function (password) {
     return bcrypt.compareSync(password, this.hash_password);
