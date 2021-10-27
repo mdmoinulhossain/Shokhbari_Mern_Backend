6c134170818c71484d8858b71915c64ab78ebd9d
unknown <za0162hid@gmail.com>
2021-10-27 15:40:14 +0600

    validation added

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -4,10 +4,15 @@ const {
   signin,
   requireSignin,
 } = require("../../controller/admin/auth");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../../validators/auth");
 const router = express.Router();
 
-router.post("/admin/signup", signup);
+router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
 
-router.post("/admin/signin", signin);
+router.post("/admin/signin", validateSigninRequest, isRequestValidated, signin);
 
 module.exports = router;
---
+++ src/validators/auth.js
@@ -0,0 +1,39 @@
+const { check, validationResult } = require("express-validator");
+
+//this is for the signup validation
+exports.validateSignupRequest = [
+  check("firstName").notEmpty().withMessage("FirstName is required"),
+  check("lastName").notEmpty().withMessage("LastName is required"),
+  check("lastName"),
+  check("email").isEmail().withMessage("Valid Email is required"),
+  check("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 character long"),
+  //   check("password")
+  //     .isLength({ min: 6 })
+  //     .withMessage("Password must be at least 6 character long"),
+];
+
+//this is for the signin validation
+exports.validateSigninRequest = [
+  check("email").isEmail().withMessage("Valid Email is required"),
+  check("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 character long"),
+  //   check("password")
+  //     .isLength({ min: 6 })
+  //     .withMessage("Password must be at least 6 character long"),
+];
+
+//request need to validate before signin/signup
+exports.isRequestValidated = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.array().length > 0) {
+    return res.status(400).json({ error: errors.array()[0].msg });
+  }
+  next();
+};
